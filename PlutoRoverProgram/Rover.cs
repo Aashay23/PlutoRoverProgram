812679eb37553121ccd482f7c42d791521367111
Aashay23 <aashay.2312@gmail.com>
2018-02-11 19:05:36 +0000

    F & B movement implementation
    Forward and Backward implementation of rover from instructions

---
+++ PlutoRoverProgram/Rover.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace PlutoRoverProgram
+{
+    public class Rover
+    {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string DirectionFacing { get; set; }
+
+        public Rover()
+        {
+            this.XCoordinate = 0;
+            this.YCoordinate = 0;
+            this.DirectionFacing = "N";
+        }
+
+        public Rover(int x, int y, string direction)
+        {
+            this.XCoordinate = x;
+            this.YCoordinate = y;
+            this.DirectionFacing = direction;
+
+        }
+
+        public void ExecuteCommand(string instructions)
+        {
+            foreach (var command in instructions)
+            {
+                switch (command)
+                {
+                    case 'F':
+                        moveRoverForward();
+                        break;
+                    case 'B':
+                        moveRoverBackward();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void moveRoverForward()
+        {
+            switch (this.DirectionFacing)
+            {
+                case "N":
+                    this.YCoordinate += 1;
+                    break;
+                case "E":
+                    this.XCoordinate += 1;
+                    break;
+                case "W":
+                    this.XCoordinate -= 1;
+                    break;
+                case "S":
+                    this.YCoordinate -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void moveRoverBackward()
+        {
+            switch (this.DirectionFacing)
+            {
+                case "N":
+                    this.YCoordinate -= 1;
+                    break;
+                case "E":
+                    this.XCoordinate -= 1;
+                    break;
+                case "W":
+                    this.XCoordinate += 1;
+                    break;
+                case "S":
+                    this.YCoordinate += 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- PlutoRoverProgram/Test.cs
+++ PlutoRoverProgram/Test.cs
@@ -5,9 +5,27 @@ namespace PlutoRoverProgram
     [TestFixture()]
     public class Test
     {
-        [Test()]
-        public void TestCase()
+        [TestCase(0,0,"N")]
+        [TestCase(1,23,"S")]
+        public void Test_RoverInitialization(int xCoord, int yCoord, string direction)
         {
+            Rover rover1 = new Rover(xCoord, yCoord, direction);
+            Assert.NotNull(rover1);
+        }
+
+        [TestCase(0, 0, "N", "FFFBB", 0, 1, "N")]
+        [TestCase(5, 10, "E", "BBBFFFF", 6, 10, "E")]
+        public void Test_MovingRoverForwardAndBackwardFromInstructions(int xCoord, int yCoord, string direction, string instructions, int finalXCoord, int finalYCoord, string finalDirection)
+        {
+            Rover plutoRover = new Rover(xCoord, yCoord, direction);
+            //string instructions = "FFFBB";
+            plutoRover.ExecuteCommand(instructions);
+
+            Rover finalLocationOfPlutoRover = new Rover(finalXCoord, finalYCoord, finalDirection);
+
+            Assert.IsTrue((plutoRover.XCoordinate == finalLocationOfPlutoRover.XCoordinate && 
+                           plutoRover.YCoordinate == finalLocationOfPlutoRover.YCoordinate && 
+                           plutoRover.DirectionFacing == finalLocationOfPlutoRover.DirectionFacing));
         }
     }
 }
